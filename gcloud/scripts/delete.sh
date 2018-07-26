ff3cbd7b4513401de60a0ea9c885a0e8dc3694aa
Hector Donovan Rincon Couret <donovan@wizeline.com>
2018-07-26 11:09:55 -0500

    Update scripts for terraform for GCP

--- gcloud/scripts/delete.sh
+++ gcloud/scripts/delete.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
+# The services account name is the one that was specified in deploy.sh
+SERVICE_ACCOUNT_NAME="test-training"
 # Put the path to the key file generated
-PATH_TO_JSON_KEY=~/gcp_key.json
+PATH_TO_JSON_KEY=~/"$SERVICE_ACCOUNT_NAME"_gcp_key.json
 export GOOGLE_APPLICATION_CREDENTIALS=$PATH_TO_JSON_KEY
 
 cd ../terraform
