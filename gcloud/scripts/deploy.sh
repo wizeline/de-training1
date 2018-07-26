98d7752883ff76b477074ea42020818dba75e13a
Hector Donovan Rincon Couret <donovan@wizeline.com>
2018-07-26 10:07:43 -0500

    Update scripts for terraform for GCP

--- gcloud/scripts/deploy.sh
+++ gcloud/scripts/deploy.sh
@@ -1,16 +1,16 @@
 #! /bin/bash
-# The services account name is the one that exists in GCP, if no account name exists, then uncomment line 12 and 13 in order to create one
-SERVICE_ACCOUNT_NAME="training"
+# The services account name is the one that exists in GCP, if not exists, it will show an error that the account exists, but there is no issue with that
+SERVICE_ACCOUNT_NAME="test-training"
 # Project active
 PROJECT_ID="data-castle-bravo"
 # Roles for the new service account
 ROLES="roles/editor"
 # This path is where the Key of the service account is stored, if no service account exists yet, it will create one in the path specified
-PATH_TO_JSON_KEY=~/gcp_key.json
+PATH_TO_JSON_KEY=~/"$SERVICE_ACCOUNT_NAME"_gcp_key.json
 
 #First, if a service account does not exists,  we need to create the service account, for that uncomment the following command
-#gcloud iam service-accounts create $SERVICE_ACCOUNT_NAME --display-name $SERVICE_ACCOUNT_NAME
-#gcloud projects add-iam-policy-binding $PROJECT_ID --member serviceAccount:$SERVICE_ACCOUNT_NAME@$PROJECT_ID.iam.gserviceaccount.com --role $ROLES
+gcloud iam service-accounts create $SERVICE_ACCOUNT_NAME --display-name $SERVICE_ACCOUNT_NAME
+gcloud projects add-iam-policy-binding $PROJECT_ID --member serviceAccount:$SERVICE_ACCOUNT_NAME@$PROJECT_ID.iam.gserviceaccount.com --role $ROLES
 
 #Then we need to get the key json file from the service account if not exists
 if ! [ -f $PATH_TO_JSON_KEY ]
