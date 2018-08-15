b4efa94547781f132b21d11b9c59c683295c5aaa
Carlos Zubieta <carlos.zubieta@wizeline.com>
2018-08-15 00:46:07 -0500

    Terraform environment heavy refactor

--- gcloud/terraform/environment.tf
+++ gcloud/terraform/environment.tf
@@ -8,9 +8,11 @@ variable "region" {}
 variable "location" {}
 
 # Buckets names
-variable "staging_prefix" {}
-variable "output_bucket_name_prefix" {}
 variable "input_bucket_name" {}
+
+variable "config_bucket_name" {}
+variable "staging_bucket_prefix" {}
+variable "output_bucket_prefix" {}
 
 # Cluster information
 variable "zeppelin_sh_path" {}
@@ -25,100 +27,352 @@ variable "cluster_worker_instances" {}
 variable "cluster_master_boot_disk_size" {}
 variable "cluster_worker_boot_disk_size" {}
 
-# Number of users
-variable "num_users" {}
-
-#User Members
-variable "user_members" {
+# Members
+variable "students" {
   type = "list"
 }
 
-// ---------------------------------
+variable "mentors" {
+  type = "list"
+}
+
+variable "admins" {
+  type = "list"
+}
+
+variable "testers" {
+  type = "list"
+}
+
+# Local variables
+locals {
+  "cluster-users"     = "${distinct(concat(var.students, var.mentors, var.testers))}"
+  "num-cluster-users" = "${length(local.cluster-users)}"
+  "users-list"        = "${formatlist("user:%s", local.cluster-users)}"
+  "admins-list"       = "${formatlist("user:%s", distinct(var.admins))}"
+  "num-admins"        = "${length(var.admins)}"
+
+  "service-accounts-students" = "${formatlist(
+    "serviceAccount:%s",
+    google_service_account.student-service-accounts.*.email
+  )}"
+}
+
+// -----------------------------------
 // Configure the Google Cloud provider
 provider "google" {
   project = "${var.project_name}"
   region  = "${var.region}"
 }
 
-// ---------------------------------  
-// Configure resources
-// Access to input bucket for each user
-resource "google_storage_bucket_iam_member" "input-bucket" {
-  bucket     = "${var.input_bucket_name}"
-  role       = "roles/storage.objectViewer"
-  member     = "user:${element(var.user_members, count.index)}"
-  count      = "${var.num_users}"
+//--------------------------
+// Mentor accounts
+
+# Owner role assignment
+resource "google_project_iam_binding" "owner-role-members" {
+  project = "${var.project_name}"
+  role    = "roles/owner"
+
+  members = [
+    "${local.admins-list}",
+  ]
 }
 
+# Storage admin role assignment
+resource "google_project_iam_binding" "storage-admin-role-members" {
+  project = "${var.project_name}"
+  role    = "roles/storage.admin"
 
-// Bucket for users to put their data/file
-resource "google_storage_bucket" "de-bucket-output" {
-  project       = "${var.project_name}"
-  name          = "${var.output_bucket_name_prefix}-${count.index}"
+  members = [
+    "${local.admins-list}",
+  ]
+}
+
+// -------------------------
+// Students Accounts
+
+# Students role
+# Allows:
+#   * Listing buckets
+#   * See vms details
+#   * SSH to vms
+#   * See dataproc cluster details
+#   * See vms and clusters resource consumption
+resource "google_project_iam_custom_role" "student-role" {
+  role_id     = "StudentRole"
+  title       = "Student Project Role"
+  description = "Custom project role for the students"
+
+  permissions = [
+    "compute.instances.get",
+    "compute.instances.list",
+    "compute.instances.osLogin",
+    "compute.instances.setMetadata",
+    "compute.machineTypes.get",
+    "compute.machineTypes.list",
+    "compute.networks.get",
+    "compute.networks.list",
+    "compute.projects.get",
+    "compute.regions.get",
+    "compute.regions.list",
+    "compute.zones.get",
+    "compute.zones.list",
+    "dataproc.clusters.get",
+    "dataproc.clusters.list",
+    "dataproc.clusters.use",
+    "dataproc.jobs.cancel",
+    "dataproc.jobs.create",
+    "dataproc.jobs.get",
+    "dataproc.jobs.list",
+    "dataproc.jobs.update",
+    "monitoring.timeSeries.list",
+    "resourcemanager.projects.get",
+    "serviceusage.quotas.get",
+    "serviceusage.services.get",
+    "serviceusage.services.list",
+    "storage.buckets.list",
+  ]
+}
+
+# Students role assignment
+resource "google_project_iam_binding" "student-role-members" {
+  project = "${var.project_name}"
+  role    = "${google_project_iam_custom_role.student-role.id}"
+
+  members = [
+    "${local.users-list}",
+  ]
+}
+
+# Students service accounts
+resource "google_service_account" "student-service-accounts" {
+  display_name = "student-${element(local.cluster-users, count.index)}"
+
+  account_id = "student-${substr(
+    replace(element(local.cluster-users, count.index), "/@.*|[^A-Za-z0-9]/", ""), 0,
+    min(20, length(replace(element(local.cluster-users, count.index), "/@.*|[^A-Za-z0-9]/", "")))
+  )}"
+
+  count = "${local.num-cluster-users}"
+}
+
+# Student service account role
+resource "google_project_iam_custom_role" "student-service-account-role" {
+  role_id     = "StudentServiceAccountRole"
+  title       = "Student Service Account Role"
+  description = "Custom role for the students to run dataproc jobs"
+
+  permissions = [
+    "dataproc.agents.create",
+    "dataproc.agents.delete",
+    "dataproc.agents.get",
+    "dataproc.agents.list",
+    "dataproc.agents.update",
+    "dataproc.tasks.lease",
+    "dataproc.tasks.listInvalidatedLeases",
+    "dataproc.tasks.reportStatus",
+    "logging.logEntries.create",
+    "monitoring.metricDescriptors.create",
+    "monitoring.metricDescriptors.get",
+    "monitoring.metricDescriptors.list",
+    "monitoring.monitoredResourceDescriptors.get",
+    "monitoring.monitoredResourceDescriptors.list",
+    "monitoring.timeSeries.create",
+    "storage.buckets.list",
+    "storage.buckets.get",
+    "storage.objects.list",
+    "storage.objects.get",
+  ]
+}
+
+resource "google_project_iam_binding" "student-service-account-role-members" {
+  project = "${var.project_name}"
+  role    = "${google_project_iam_custom_role.student-service-account-role.id}"
+
+  members = [
+    "${local.service-accounts-students}",
+  ]
+}
+
+# Student service account role assignment
+resource "google_service_account_iam_member" "student-service-account-users" {
+  service_account_id = "${element(google_service_account.student-service-accounts.*.id, count.index)}"
+  role               = "roles/iam.serviceAccountUser"
+  member             = "${element(local.users-list, count.index)}"
+  count              = "${local.num-cluster-users}"
+}
+
+// -----------------------------------------------------------------------------
+// Buckets
+# Input bucket
+resource "google_storage_bucket" "input-bucket" {
+  project  = "${var.project_name}"
+  name     = "${var.input_bucket_name}"
+  location = "${var.location}"
+
+  lifecycle {
+    prevent_destroy = "true"
+  }
+}
+
+# Configuration bucket
+resource "google_storage_bucket" "config-bucket" {
+  project  = "${var.project_name}"
+  name     = "${var.config_bucket_name}"
+  location = "${var.location}"
+
+  lifecycle {
+    prevent_destroy = "true"
+  }
+}
+
+# Bucket for users to put their data/file
+resource "google_storage_bucket" "output-buckets" {
+  project = "${var.project_name}"
+
+  name = "${var.output_bucket_prefix}-${substr(
+    replace(element(local.cluster-users, count.index), "/@.*|[^A-Za-z0-9]/", ""), 0,
+    min(20, length(replace(element(local.cluster-users, count.index), "/@.*|[^A-Za-z0-9]/", "")))
+  )}"
+
+  location = "${var.location}"
+  count    = "${local.num-cluster-users}"
+
+  lifecycle {
+    prevent_destroy = "false"
+  }
+}
+
+# Bucket required for the dataproc cluster
+resource "google_storage_bucket" "staging-buckets" {
+  project = "${var.project_name}"
+
+  name = "${var.staging_bucket_prefix}-${substr(
+    replace(element(local.cluster-users, count.index), "/@.*|[^A-Za-z0-9]/", ""), 0,
+    min(20, length(replace(element(local.cluster-users, count.index), "/@.*|[^A-Za-z0-9]/", "")))
+  )}"
+
   location      = "${var.location}"
+  count         = "${local.num-cluster-users}"
   force_destroy = "true"
-  count         = "${var.num_users}"
 }
 
-// Access to users for buckets, the list in environment vars goes from index 0 to n-1 (same as the buckets)
-resource "google_storage_bucket_iam_member" "viewer" {
-  bucket     = "${var.output_bucket_name_prefix}-${count.index}"
-  role       = "roles/storage.objectViewer"
-  member     = "user:${element(var.user_members, count.index)}"
-  count      = "${var.num_users}"
-  depends_on = ["google_storage_bucket.de-bucket-output"]
+// ------------------------
+// Bucket level permissions
+# Input bucket
+resource "google_storage_bucket_iam_binding" "input-bucket-obj-viewers" {
+  bucket = "${google_storage_bucket.input-bucket.name}"
+  role   = "roles/storage.objectViewer"
+
+  members = [
+    "${concat(local.users-list, local.service-accounts-students)}",
+  ]
 }
 
-resource "google_storage_bucket_iam_member" "create" {
-  bucket     = "${var.output_bucket_name_prefix}-${count.index}"
-  role       = "roles/storage.objectCreator"
-  member     = "user:${element(var.user_members, count.index)}"
-  count      = "${var.num_users}"
-  depends_on = ["google_storage_bucket.de-bucket-output"]
+# Config bucket
+resource "google_storage_bucket_iam_binding" "config-bucket-obj-viewers" {
+  bucket = "${google_storage_bucket.config-bucket.name}"
+  role   = "roles/storage.objectViewer"
+
+  members = [
+    "${local.service-accounts-students}",
+  ]
 }
 
-// Bucket required for the cluster
-resource "google_storage_bucket" "de-staging" {
-  project       = "${var.project_name}"
-  name          = "${var.staging_prefix}-${count.index}"
-  location      = "${var.location}"
-  force_destroy = "true"
-  count         = "${var.num_users}"
+# Output buckets
+resource "google_storage_bucket_iam_binding" "output-bucket-obj-viewers" {
+  bucket = "${element(google_storage_bucket.output-buckets.*.name, count.index)}"
+  role   = "roles/storage.objectViewer"
+
+  members = [
+    "${element(local.users-list, count.index)}",
+    "${element(local.service-accounts-students, count.index)}",
+  ]
+
+  count = "${local.num-cluster-users}"
 }
 
-// Custom Student, Service Account User and OS Login roles that allows to work with dataprocs for users
-resource "google_project_iam_member" "student-role" {
-  project = "${var.project_name}"
-  role    = "projects/data-castle-bravo/roles/StudentRole"
-  member  = "user:${element(var.user_members, count.index)}"
-  count   = "${var.num_users}"
+resource "google_storage_bucket_iam_binding" "output-bucket-obj-creators" {
+  bucket = "${element(google_storage_bucket.output-buckets.*.name, count.index)}"
+  role   = "roles/storage.objectCreator"
+
+  members = [
+    "${element(local.users-list, count.index)}",
+  ]
+
+  count = "${local.num-cluster-users}"
 }
-resource "google_project_iam_member" "service-account-user-role" {
-  project = "${var.project_name}"
-  role    = "roles/iam.serviceAccountUser"
-  member  = "user:${element(var.user_members, count.index)}"
-  count   = "${var.num_users}"
+
+resource "google_storage_bucket_iam_binding" "output-bucket-obj-admins" {
+  bucket = "${element(google_storage_bucket.output-buckets.*.name, count.index)}"
+  role   = "roles/storage.objectAdmin"
+
+  members = [
+    "${element(local.service-accounts-students, count.index)}",
+  ]
+
+  count = "${local.num-cluster-users}"
 }
-resource "google_project_iam_member" "os-login-role" {
-  project = "${var.project_name}"
-  role    = "roles/compute.osLogin"
-  member  = "user:${element(var.user_members, count.index)}"
-  count   = "${var.num_users}"
+
+# Staging buckets
+resource "google_storage_bucket_iam_binding" "staging-bucket-obj-viewers" {
+  bucket = "${element(google_storage_bucket.staging-buckets.*.name, count.index)}"
+  role   = "roles/storage.objectViewer"
+
+  members = [
+    "${element(local.users-list, count.index)}",
+  ]
+
+  count = "${local.num-cluster-users}"
 }
+
+resource "google_storage_bucket_iam_binding" "staging-bucket-obj-creators" {
+  bucket = "${element(google_storage_bucket.staging-buckets.*.name, count.index)}"
+  role   = "roles/storage.objectCreator"
+
+  members = [
+    "${element(local.users-list, count.index)}",
+  ]
+
+  count = "${local.num-cluster-users}"
+}
+
+resource "google_storage_bucket_iam_binding" "staging-bucket-obj-admins" {
+  bucket = "${element(google_storage_bucket.staging-buckets.*.name, count.index)}"
+  role   = "roles/storage.objectAdmin"
+
+  members = [
+    "${element(local.service-accounts-students, count.index)}",
+  ]
+
+  count = "${local.num-cluster-users}"
+}
+
+// -----------------------------------------------------------------------------
+// Dataproc
 
 // Cluster that is initialized with zeppelin.sh previously copied.
-resource "google_dataproc_cluster" "de-training" {
-  name       = "${var.cluster_prefix}-${count.index}"
-  region     = "${var.region}"
-  count      = "${var.num_users}"
-  depends_on = ["google_storage_bucket.de-bucket-output"]
+resource "google_dataproc_cluster" "spark-clusters" {
+  name = "${var.cluster_prefix}-${substr(
+    replace(element(local.cluster-users, count.index), "/@.*|[^A-Za-z0-9]/", ""), 0,
+    min(20, length(replace(element(local.cluster-users, count.index), "/@.*|[^A-Za-z0-9]/", "")))
+  )}"
+
+  region = "${var.region}"
+  count  = "${local.num-cluster-users}"
+
+  depends_on = [
+    "google_storage_bucket.config-bucket",
+    "google_storage_bucket_iam_binding.config-bucket-obj-viewers",
+    "google_project_iam_binding.student-service-account-role-members",
+  ]
 
   cluster_config {
-    // Staging bucket name previously created
-    staging_bucket = "${var.staging_prefix}-${count.index}"
+    staging_bucket = "${element(google_storage_bucket.staging-buckets.*.name, count.index)}"
 
     gce_cluster_config {
-      zone = "${var.zone}"
+      zone            = "${var.zone}"
+      service_account = "${element(google_service_account.student-service-accounts.*.email, count.index)}"
     }
 
     master_config {
@@ -144,7 +398,9 @@ resource "google_dataproc_cluster" "de-training" {
     # You can define multiple initialization_action blocks
     initialization_action {
       // Path to the bucket where the zeppelin.sh was copied
-      script      = "${var.zeppelin_sh_path}"
+      /* script      = "${google_storage_bucket.config-bucket.url}/${var.zeppelin_sh_path}" */
+      script = "gs://dataproc-initialization-actions/zeppelin/zeppelin.sh"
+
       timeout_sec = "${var.cluster_init_timeout}"
     }
   }
--- gcloud/terraform/environment.tfvars
+++ gcloud/terraform/environment.tfvars
@@ -1,50 +1,96 @@
-# Project information
-
+# Project configuration
 project_name = "data-castle-bravo"
-
 region = "us-central1"
-
 zone = "us-central1-a"
-
 location = "US"
 
 # Buckets names
+config_bucket_name = "de-training-config"
+input_bucket_name = "de-training-input"
+staging_bucket_prefix = "de-training-staging"
+output_bucket_prefix = "de-training-output"
 
-input_bucket_name = "de-training-input-bucket"
-
-staging_prefix = "de-training-staging"
-
-output_bucket_name_prefix = "de-training-output-bucket"
-
-#Cluster information
-
-zeppelin_sh_path = "gs://dcb-initialization-actions/zeppelin.sh"
+# Clusters configuration
+zeppelin_sh_path = "zeppelin/zeppelin.sh"
 
 cluster_prefix = "de-training"
-
 machine_type = "n1-standard-2"
-
 cluster_master_num_local_ssds = 0
-
 cluster_worker_num_local_ssds = 0
-
 cluster_init_timeout = 500
-
 cluster_master_instances = 1
-
 cluster_worker_instances = 2
-
 cluster_master_boot_disk_size = 15
-
 cluster_worker_boot_disk_size = 10
 
-# Number of users
-num_users = 1
+# Admins list
+admins = [
+  "abraham.alcantara@wizeline.com",
+  "ana.gabriela@wizeline.com",
+  "carlos.zubieta@wizeline.com",
+  "donovan@wizeline.com",
+  "edgar.arenas@wizeline.com",
+  "luis.dealba@wizeline.com",
+  "matthew.ropp@wizeline.com",
+  "ricardo.magana@wizeline.com",
+  "rodrigo.chaparro@wizeline.com",
+  "said.montiel@wizeline.com",
+  "willebaldo@wizeline.com",
+]
+
+# Mentors list
+mentors = [
+  "abraham.alcantara@wizeline.com",
+  "ana.gabriela@wizeline.com",
+  "carlos.zubieta@wizeline.com",
+  "edgar.arenas@wizeline.com",
+  "ricardo.magana@wizeline.com",
+  "rodrigo.chaparro@wizeline.com",
+  "said.montiel@wizeline.com",
+  "willebaldo@wizeline.com",
+]
+
+# Testers list
+testers = [
+  "matthew.ropp@wizeline.com",
+  "leobiwankenobi@gmail.com",
+]
 
 # Service Accounts list
 # The number of accounts should match with the number of users
 #
 # This is an example, update with the users gmail accounts. index 0 goes to bucket 0
-user_members = [
-    "data-cartel@wizeline.com",
+students = [
+  "alberto.leal@wizeline.com",
+  "alfre2x@gmail.com",
+  "armando.diaz.glez@gmail.com",
+  "aztre@yellowme.mx",
+  "barpan@gmail.com",
+  "brunoarh@gmail.com",
+  "ceoe1996@gmail.com",
+  "ces.nietor@gmail.com",
+  "chuygc@gmail.com",
+  "diego@tr3sco.com",
+  "eejl1992@gmail.com",
+  "efren.ce@gmail.com",
+  "fherdelpino@gmail.com",
+  "francisco.gonzalez.vazquez@gmail.com",
+  "gmisaenz@gmail.com",
+  "ivo@wizeline.com",
+  "jalil@relativity6.com",
+  "javier.guajardo@wizeline.com",
+  "javiero.barbosa@gmail.com",
+  "joaquincasanova001@gmail.com",
+  "jorge.vera.colima@gmail.com",
+  "jorge@yellowme.mx",
+  "martinez.zapata.carlos@gmail.com",
+  "migueltlapa@gmail.com",
+  "nunez.gonzalez.jl@gmail.com",
+  "petergtam@gmail.com",
+  "rhdzmota@gmail.com",
+  "rjlopez.android@gmail.com",
+  "saul.flores@wizeline.com",
+  "sjcmexr@gmail.com",
+  "soysamygp@gmail.com",
+  "topifloresl@gmail.com",
 ]
